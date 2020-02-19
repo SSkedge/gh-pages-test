00b503945c46ddfcc59ea33b1086ac58fda8c530
SSkedge <19799750+SSkedge@users.noreply.github.com>
2020-02-19 13:04:01 +0000

    Updates

---
+++ precache-manifest.cb5819a4e2683b2973f8a45537e2c046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c52eb570087e1cba7024b327669261",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927f43dd0c523f1f1496",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d903c4e79226cc5c5da9",
+    "url": "/static/js/2.ce0dac48.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ce0dac48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927f43dd0c523f1f1496",
+    "url": "/static/js/main.e5fc5497.chunk.js"
+  },
+  {
+    "revision": "821341eb31157e562a95",
+    "url": "/static/js/runtime-main.7909cbb1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
