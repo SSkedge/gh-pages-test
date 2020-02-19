6781fe7d5807b32e57e7ee2dbaf97b7c25e2174a
SSkedge <19799750+SSkedge@users.noreply.github.com>
2020-02-19 13:23:58 +0000

    Updates

---
+++ precache-manifest.b0086ddc3ab5b670fd9d3ae98febb6c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3349b0409bfbf6a0a71d69012a154971",
+    "url": "/gh-pages-test/index.html"
+  },
+  {
+    "revision": "7387f7b26b79da3adc35",
+    "url": "/gh-pages-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d903c4e79226cc5c5da9",
+    "url": "/gh-pages-test/static/js/2.ce0dac48.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gh-pages-test/static/js/2.ce0dac48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7387f7b26b79da3adc35",
+    "url": "/gh-pages-test/static/js/main.e9baa012.chunk.js"
+  },
+  {
+    "revision": "ff9f7b9a067fd940f6ed",
+    "url": "/gh-pages-test/static/js/runtime-main.b24569a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
